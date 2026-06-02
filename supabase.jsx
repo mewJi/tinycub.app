91027a5742350755da5cafcc0b430c06735863f9
mewJi <meme.doubleu@gmail.com>
2026-06-02 16:55:53 +0700

    Update supabase.jsx

--- supabase.jsx
+++ supabase.jsx
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_URL";
-const SUPABASE_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://atlpsgdwhwjmmxcmecsa.supabase.co/rest/v1/";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0bHBzZ2R3aHdqbW14Y21lY3NhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzNzkxODMsImV4cCI6MjA5NTk1NTE4M30.q7b11JzvXWvTnVL95KfFFvG8EySUhmfCOQML7MjwXiU";
 
 window.supabaseClient =
   supabase.createClient(
